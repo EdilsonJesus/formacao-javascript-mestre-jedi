118fc43cb24387afb5db99c73c55bf8917ecf008
Edilson Jesus <edilsondevelop@gmail.com>
2023-02-02 22:56:07 -0300

    Realizando a primeira missão

---
+++ missao-estelar/primeira-missao/EdilsonJesus/Readme.md
@@ -0,0 +1,11 @@
+## Missão n° 1 
+
+
+### Minhas principais tecnologias
+
+ - Java 
+ - PHP 
+ - Javascript
+ - CSS 
+ - HTML  
+
---
+++ missao-estelar/primeira-missao/EdilsonJesus/js/scripts.js
@@ -0,0 +1,9 @@
+var missao = "Missão Estelar";
+
+function eventoOnload(){
+	console.log("Chamando o evento");	
+};
+
+function eventoOnclick(){
+	console.log("Missão concluida com sucesso!");
+}
